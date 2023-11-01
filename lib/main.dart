d0128eaa9e36890b8bc7bf7575d3eb0abd11b43f
Cristian Rubi <rcristianrubi@gmail.com>
2023-10-31 20:39:03 -0600

    Added Serial port setUp and modified function to write in the serial port

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  MainApp({super.key});
+  final port = SerialPort('COM3');
+
+  //setup Serial
 
   void writeSerial() {
-    print('Serial');
+    port.write(Uint8List.fromList("101001".codeUnits));
   }
 
   @override
   Widget build(BuildContext context) {
+    port.openReadWrite();
+    port.config = SerialPortConfig()
+      ..baudRate = 115200
+      ..bits = 8
+      ..parity = SerialPortParity.none
+      ..stopBits = 1
+      ..setFlowControl(SerialPortFlowControl.none);
+
     return MaterialApp(
       home: Scaffold(
         body: Center(
